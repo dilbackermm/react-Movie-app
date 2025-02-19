ce4d8e5930deb3f66f591d27a49655935690c294
dilbacker <mmdilbacker@gmail.com>
2025-02-19 12:39:38 +0530

    creating movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const Card = ({ title }) => {
+
+  const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect( () => {
+console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked]);
+
+  return(
+    <div className="card"  onClick={ () => setCount(count + 1)}>
+    <h2 >{title} <br/> {count} </h2>
+    <button onClick={() => setHasLiked(!hasLiked)}>
+     { hasLiked ? "❤️" : "🤍"}
+    </button>
+    </div>
+
+  ) 
+};
 
 const App = () => {
-  return(
-    <h2>Functional Arrow component</h2>
-  )
-}
+  return (
+    <>
+      <h2>Functional Arrow component</h2>
+      <div className="card-container">
+        <Card title="Showshank redemption" />
+        <Card title="Forrest Gump" />
+        <Card title="Interstellar" />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
