2a348256f9d6be5419a94609391765deb4e95f9b
dilbacker <mmdilbacker@gmail.com>
2025-02-20 18:00:09 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Search from "./components/Search";
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
@@ -22,18 +23,23 @@ const Card = ({ title }) => {
 };
 
 const App = () => {
+
+const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <main>
-      <div className="pattern">
+      <div className="pattern" />
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="Hero Banner" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without Hassle
             </h1>
           </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h3 className="text-white">{searchTerm}</h3>
         </div>
-      </div>
     </main>
   );
 };
