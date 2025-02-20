e32e4e40309d1dd961595b60796e3d08213e1fce
dilbacker <mmdilbacker@gmail.com>
2025-02-20 11:15:29 +0530

    adding public assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,39 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const Card = ({ title }) => {
-
   const [hasLiked, setHasLiked] = useState(false);
   const [count, setCount] = useState(0);
 
-  useEffect( () => {
-console.log(`${title} has been liked: ${hasLiked}`)
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
   }, [hasLiked]);
 
-  return(
-    <div className="card"  onClick={ () => setCount(count + 1)}>
-    <h2 >{title} <br/> {count} </h2>
-    <button onClick={() => setHasLiked(!hasLiked)}>
-     { hasLiked ? "❤️" : "🤍"}
-    </button>
+  return (
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} <br /> {count}{" "}
+      </h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
     </div>
-
-  ) 
+  );
 };
 
 const App = () => {
   return (
-    <>
-      <h2>Functional Arrow component</h2>
-      <div className="card-container">
-        <Card title="Showshank redemption" />
-        <Card title="Forrest Gump" />
-        <Card title="Interstellar" />
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without Hassle
+            </h1>
+          </header>
+        </div>
       </div>
-    </>
+    </main>
   );
 };
 
